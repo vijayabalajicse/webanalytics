1b2f6adb0f326492e04e8a63658ae13160ed5034
vijayabalajicse <vijaya.balaji@a-cti.com>
2014-01-08 20:42:33 +0530

    working after some changes

---
+++ src/com/analytics/auth/DemoClass.java
@@ -0,0 +1,12 @@
+package com.analytics.auth;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoClass {
+
+	
+	public static void printdata(HttpServletRequest req, HttpServletResponse resp){
+		System.out.println("print data");
+	}
+}
