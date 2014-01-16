5b5b928d6f8fd420f4806d9967a8dfdc528a40d4
vijayabalajicse <vijaya.balaji@a-cti.com>
2014-01-16 21:54:51 +0530

    Correction in Ajax

--- src/com/google/webAnalytics/WebAnalyticsServlet.java
+++ src/com/google/webAnalytics/WebAnalyticsServlet.java
@@ -4,10 +4,8 @@ package com.google.webAnalytics;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -21,30 +19,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
@@ -62,12 +36,6 @@ import com.google.api.services.analytics.model.Profiles;
 import com.google.api.services.analytics.model.Webproperties;
 import com.google.api.services.oauth2.Oauth2;
 import com.google.api.services.oauth2.model.Userinfo;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 
 
 @Controller
@@ -81,45 +49,38 @@ public class WebAnalyticsServlet  {
     Analytics analytics;
     ObjectMapper jsonMap = new ObjectMapper();
     JSONObject jsonObj = new JSONObject();
-	JSONParser parser = new JSONParser();
-    
-	Logger log = Logger.getLogger("LogInfo");
-	static final String KIND = "AnalticsObject";
+	JSONParser parser = new JSONParser();    
+	Logger log = Logger.getLogger("LogInfo");	
+	
 	@RequestMapping("/oauth2callback")
 	public String callback(HttpServletRequest req, HttpServletResponse resp,ModelMap model){
 		
 		Credential credential;
 		String code = req.getParameter("code");
 		Oauth2 userService;
-		Userinfo userInfo;	
+		Userinfo userInfo;
+		HttpSession session;
+		String emailid;
+		Accounts account;
 		
-		if(req.getParameter("code") != null || req.getParameter("code") != ""){
-			   
+		if(req.getParameter("code") != null || req.getParameter("code") != ""){			   
 			credential = getCredential(code);
 			userService = new Oauth2.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Web Analytics").build();
 			try {
 				userInfo = userService.userinfo().get().execute();
-				String emailid =  userInfo.getEmail();
-				HttpSession session=req.getSession(); 
-				
+				emailid =  userInfo.getEmail();
+				session=req.getSession(); 				
 				session.setAttribute("SESSION_USEREMAILID", emailid);
 				session.setAttribute("USER_ACCESSTOKEN", credential.getAccessToken());
 				session.setAttribute("USER_REFRESHTOKEN", credential.getRefreshToken());
 				session.setAttribute("EXPIRES_MILISECOND", credential.getExpirationTimeMilliseconds());
-				session.setAttribute("USER_AUTH_CODE", code);
-			//	storeCredential(emailid,credential);
-				
+				session.setAttribute("USER_AUTH_CODE", code);				
 				analytics = new Analytics.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Web Analytics").build();
-				Accounts account = analytics.management().accounts().list().execute();
-			//	session.setAttribute("SessionGadate", account.);
-				log.info(account.getItems().get(0).getName());
-				//session.setAttribute("SESSION_OBJ" );
-				//session.setAttribute("USERCredential", analytics);
-				//storeAnalyticsObject(emailid,analytics.toString());
+				account= analytics.management().accounts().list().execute();			
+				log.info(account.getItems().get(0).getName());				
 				System.out.println("Analytics Obj: "+analytics);
 				System.out.println("Refresh token: "+ credential.getRefreshToken());
-				location = "redirect:/home";
-				
+				location = "redirect:/home";				
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -138,8 +99,6 @@ public class WebAnalyticsServlet  {
 		    log.warning("code not get");
 			
 		}
-		
-		
 		return location;
 		
 	}
@@ -148,64 +107,67 @@ public class WebAnalyticsServlet  {
 
 	
 
-
+//Redirection Home Page
 @RequestMapping("/home")
 public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,ModelMap model){
-	HttpSession session =  req.getSession(false);
-	String urlLocation = null;
-	if(session == null){
-    try {
-		resp.sendRedirect("/index.html");
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	}
-	else if(session.getAttribute("SESSION_USEREMAILID") == null || session.getAttribute("SESSION_USEREMAILID") == ""){
- 		
- 		urlLocation = "logout";
+		HttpSession session =  req.getSession(false);
+		String urlLocation = null;
+		if(session == null){
+			try {
+				resp.sendRedirect("/index.html");
+			} catch (IOException e) {
+				System.out.println("Problem in response redirection");
+			}
+		}
+		else if(session.getAttribute("SESSION_USEREMAILID") == null || session.getAttribute("SESSION_USEREMAILID") == ""){
+			urlLocation = "logout";
 		
- 	}
- 	else{
- 		urlLocation = "home";
- 	}
+		}
+		else{
+			urlLocation = "home";
+		}
 	
 	return urlLocation;
 }
+
+     //Send the Authroziation code and getting the accesstoken, creating Credential Object
 	private Credential getCredential(String code) {
-		// TODO Auto-generated method stub
+		GoogleTokenResponse tokenResponse;
 		try {			
 			System.out.println("Authorization code: "+code);
-			GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(HTTP_TRANSPORT,JSON_FACTORY,clientSecrets().getWeb().getClientId(),clientSecrets().getWeb().getClientSecret(),code,clientSecrets().getWeb().getRedirectUris().get(0)).execute();
+			tokenResponse = new GoogleAuthorizationCodeTokenRequest(HTTP_TRANSPORT,JSON_FACTORY,clientSecrets().getWeb().getClientId(),clientSecrets().getWeb().getClientSecret(),code,clientSecrets().getWeb().getRedirectUris().get(0)).execute();
 			return new GoogleCredential.Builder().setClientSecrets(clientSecrets()).setJsonFactory(JSON_FACTORY).setTransport(HTTP_TRANSPORT).build().setAccessToken(tokenResponse.getAccessToken()).setRefreshToken(tokenResponse.getRefreshToken());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Problem in sending code or creating credential object");
 		}
 		return null;
 	}
 
+	//Login URL
 	@RequestMapping("/googleLogin")
 	public void googleLoginUrl(HttpServletResponse response){
 		try {
 			response.sendRedirect(getBuildinLoginUrl());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException e) {			
+			System.out.println("Probelm in response redirection");
 		}
 		
 	}
-	
+	//Handle Ajax url request for get the Property info
 	@RequestMapping("/getPropertyInfo")
 	public @ResponseBody String getPropertyInfo(@RequestBody String ajaxdata,HttpServletRequest req){
-		
-		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
-		Analytics analytics = getAnalayticsObj(accessToken);
-		Map propertiesInfo =  new LinkedHashMap();
-		String accountId =ajaxdata.substring(0, ajaxdata.length()-1);
+		Webproperties webProperties;
+		Analytics analytics;
+		String accessToken;
+		String accountId;
 		String properties ="";
+		Map<String,String> propertiesInfo =  new LinkedHashMap<String,String>();
+		accessToken= (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
+		analytics= getAnalayticsObj(accessToken);
+		System.out.println(ajaxdata);
+		accountId=ajaxdata.substring(0, ajaxdata.length()-1);		
 		try {
-			Webproperties webProperties = analytics.management().webproperties().list(accountId).execute();
+			 webProperties = analytics.management().webproperties().list(accountId).execute();
 			if(webProperties.getItems().isEmpty()){
 				System.out.println("No data found");
 			}else{
@@ -216,38 +178,39 @@ public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,Model
 				System.out.println(properties);
 			}
 				
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException e) {			
+			System.out.println("Problem in getting the WebProperties from Analytics");
 		}
 		 return properties;
 	}
 	
-	@SuppressWarnings("unchecked")
+	//Handle Ajax url request for get the Profile info
 	@RequestMapping("/getProfileInfo")
 	public @ResponseBody String getProfileInfo(@RequestBody String propData,HttpServletRequest req){
-		Map profileMap = new LinkedHashMap();
+		Map<String,String> profileMap = new LinkedHashMap<String,String>();
 		String profileInfo ="";
+		Analytics analytics;
+		Profiles profile;
+		String accessToken, propertyId,accountId;
 		System.out.println(propData);
 		try {
 			
 			jsonObj = (JSONObject) parser.parse(propData);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (ParseException e) {			
+			System.out.println("Problem in parse the json Data");
 		}
 		
 		System.out.println(jsonObj.get("AccountId"));
 		System.out.println(jsonObj.get("PropertiesID"));
-		String accountId = (String) jsonObj.get("AccountId");
-		String propertyId= (String) jsonObj.get("PropertiesID");
-		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
-		Analytics analytics = getAnalayticsObj(accessToken);
+		accountId  = (String) jsonObj.get("AccountId");
+		propertyId = (String) jsonObj.get("PropertiesID");
+		accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
+		analytics = getAnalayticsObj(accessToken);
 		try {
-			Profiles profile = analytics.management().profiles().list(accountId, propertyId).execute();
-			if(profile.getItems().isEmpty()){
-				System.out.println("Empty");
-			}else{
+			 profile = analytics.management().profiles().list(accountId, propertyId).execute();
+				if(profile.getItems().isEmpty()){
+					System.out.println("Empty");
+				}else{
 				for(int i=0;i< profile.getItems().size();i++){
 					profileMap.put(profile.getItems().get(i).getId(),profile.getItems().get(i).getName());
 				}
@@ -264,70 +227,34 @@ public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,Model
 		return profileInfo;
 	}
 
+	//User logout
 	@RequestMapping("/logout")
 	public String logout(HttpServletRequest req, HttpServletResponse resp){
 		HttpSession session = req.getSession(false);
-		session.invalidate();
-		
+		session.invalidate();		
 		return "logout";
 	}
 	
+	
+	//Handle Ajax url request for get the account info
 	@RequestMapping("/getAccountInfo")
 	public @ResponseBody String getAccountInfo(HttpServletRequest req){
 		log.info("return the acccountinfo");
-		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
-		//String refreshcode = (String) req.getSession().getAttribute("USER_REFRESHTOKEN");
-		//long expiresMiliSeconds = (long) req.getSession().getAttribute("EXPIRES_MILISECOND");
-		//System.out.println(accessToken + "   " );
-      //  Credential credential = new GoogleCredential.Builder().setClientSecrets(clientSecrets()).setJsonFactory(JSON_FACTORY).setTransport(HTTP_TRANSPORT).build();
-       // credential.setAccessToken(accessToken);
-      //  credential.setRefreshToken(refreshcode);
-      //  credential.setExpirationTimeMilliseconds(expiresMiliSeconds);		
-	Analytics analytics = getAnalayticsObj(accessToken);	
-	//Analytics analytics_1 = (Analytics) req.getSession().getAttribute("SESSION_OBJ");	
-	System.out.println("Analytics obj: "+analytics);	
-	//System.out.println("Analytics client: " +analytics.getGoogleClientRequestInitializer());
-	//System.out.println("Analytics Reg: "+analytics.getRequestFactory());
-	String 	accountInfo = getProfileId(analytics);
+		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");			
+		Analytics analytics = getAnalayticsObj(accessToken);	
+		System.out.println("Analytics obj: "+analytics);	
+		String 	accountInfo = getAccountInfo(analytics);
 	 return accountInfo;	
 	}
 	
-	private Analytics getAnalaytics(String emailid) {
-		Credential credential = new GoogleCredential.Builder().setClientSecrets(clientSecrets()).setJsonFactory(JSON_FACTORY).setTransport(HTTP_TRANSPORT).build();
-		Analytics analytics = null;
-		if(getCredentialformStore(emailid,credential)){
-			analytics = new Analytics.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).build();
-		}
-		
-		
-		
-		return  analytics;
-	}
+	
 
-
-	private boolean getCredentialformStore(String emailid, Credential credential) {
-		// TODO Auto-generated method stub
-		DatastoreService getcredential = DatastoreServiceFactory.getDatastoreService();
-		Key key = KeyFactory.createKey(KIND, emailid);
-		try {
-			Entity entity = getcredential.get(key);
-			credential.setAccessToken((String) entity.getProperty("accessToken"));
-			credential.setRefreshToken((String) entity.getProperty("refreshToken"));
-			return true;
-			
-		} catch (EntityNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}	
-		
-		
-		
-	}
+	
+	//Creating the Analytics Object 
     private Analytics getAnalayticsObj(String accesstoken){
     	Credential credential = new GoogleCredential.Builder().setClientSecrets(clientSecrets()).setJsonFactory(JSON_FACTORY).setTransport(HTTP_TRANSPORT).build();
     	credential.setAccessToken(accesstoken);
-    	Analytics analytics = new Analytics.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).build();
+    	Analytics analytics = new Analytics.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Web Anlaytics").build();
     	return analytics;
     	
     }
@@ -335,40 +262,33 @@ public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,Model
 	@RequestMapping("/getdataAjax")
 	public @ResponseBody String getData(@RequestBody String data,HttpServletRequest req) {
 		
-		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");
-		//String refreshcode = (String) req.getSession().getAttribute("USER_REFRESHTOKEN");
-	//	long expiresMiliSeconds = (long) req.getSession().getAttribute("EXPIRES_MILISECOND");
-		System.out.println(accessToken + "   " );
-      //  Credential credential = new GoogleCredential.Builder().setClientSecrets(clientSecrets()).setJsonFactory(JSON_FACTORY).setTransport(HTTP_TRANSPORT).build();
-       // credential.setAccessToken(accessToken);
-       // credential.setRefreshToken(refreshcode);
-      //  credential.setExpirationTimeMilliseconds(expiresMiliSeconds);		
-		Analytics analytics = getAnalayticsObj(accessToken);
-		//String table_id,metrics,dimension,startDate,endDate;
-		log.info("Parsing the data from clent");
-		//JSONObject jsonObj = new JSONObject();
-		//JSONParser parser = new JSONParser();
+		String accessToken = (String) req.getSession().getAttribute("USER_ACCESSTOKEN");	
+		System.out.println("Accesstoken"+accessToken);      	
+		Analytics analytics = getAnalayticsObj(accessToken);		
+		log.info("Parsing the data from clent");		
 		String data_result = null;
 		try
 		{
-		jsonObj = (JSONObject) parser.parse(data);
+		jsonObj = (JSONObject) parser.parse(data);		
+		}
+		catch(ParseException e)
+		{
+			e.printStackTrace();
+		}
 		String table_id = (String) jsonObj.get("tableid");
 		String metrics = (String) jsonObj.get("metrics");
 		String dimension = (String) jsonObj.get("dimension");
 		String startDate =  (String) jsonObj.get("startDate");
 		String endDate =  (String) jsonObj.get("endDate");
-		System.err.println(table_id + " " + metrics + " " + dimension + " " + startDate + " " + endDate);
+		System.out.println(table_id + " " + metrics + " " + dimension + " " + startDate + " " + endDate);
 		data_result = getResultsData(table_id,metrics,dimension,startDate,endDate,analytics);
-		System.out.println(data_result);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+		//System.out.println(data_result);
+		
 		return data_result;
 		
 	}
 	
+	//Getting the GaData from Anlaytics
 	private String getResultsData(String table_id, String metrics,String dimension, String startDate, String endDate,Analytics analytics) 
 	{
 	
@@ -383,17 +303,19 @@ public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,Model
 	return value;
 	}
 	
+	//Getting Client Secrects form  json file
 	static private GoogleClientSecrets clientSecrets(){
 		try{
 			return GoogleClientSecrets.load(JSON_FACTORY,new InputStreamReader(WebAnalyticsServlet.class.getResourceAsStream("client_secrets.json")));
 		}catch(Exception e){
-			
+			System.out.println("Problem in getting Client Secrets");
 		}
 		return null;
 		
 	}
-	static private String getBuildinLoginUrl() {
-		// TODO Auto-generated method stub
+	
+	//Building Login Url
+	static private String getBuildinLoginUrl() {		
 		scopes.add(AnalyticsScopes.ANALYTICS_READONLY);
         scopes.add("https://www.googleapis.com/auth/userinfo.email");
         url = new GoogleAuthorizationCodeRequestUrl(clientSecrets().getWeb().getClientId(),clientSecrets().getWeb().getRedirectUris().get(0),scopes).setAccessType("offline");
@@ -401,38 +323,27 @@ public String homeRedirect(HttpServletRequest req,HttpServletResponse resp,Model
 	}
 	
 	
-	
-	private String getProfileId(Analytics analytics)  
+	//Get the Account info from google anlytics 
+	private String getAccountInfo(Analytics analytics)  
 	{
 		log.info("Get profile info");
-		Map accountNameInfo = new LinkedHashMap();
-		ObjectMapper jsonMap = new ObjectMapper();
-		List list = new ArrayList();
-		//String profileId = null;
-		String accountInfo = "";
-		//String accountName ="",webProp = "",profileName ="",profId="",accountId="",propId="",defaultvalue="";
+		Map<String,String> accountNameInfo = new LinkedHashMap<String,String>();
+		ObjectMapper jsonMap = new ObjectMapper();		
+		String accountInfo = "";		
 		try
 		{
 			Accounts account = analytics.management().accounts().list().execute();
 			if(account.getItems().isEmpty()){
 			System.out.println("Sorry no Account founded");
-			System.err.println("Account name: "+account.getItems().get(0).getName());
+			System.out.println("Account name: "+account.getItems().get(0).getName());
 			log.info("No Account found");
 			}
 			else{
-				for ( int i =0; i< account.getItems().size();i++){
-					//accountNameInfo.put( "AccountName: ", "\""+i"\"");
-					accountNameInfo.put(account.getItems().get(i).getId(), account.getItems().get(i).getName());
-					
-					//list.add(account.getItems().get(i).getId(), account.getItems().get(i).getName());
-					//list.add(account.getItems().get(i).setId(id), element);
-				}
-				//list = account.getItems();
+				for ( int i =0; i< account.getItems().size();i++){					
+					accountNameInfo.put(account.getItems().get(i).getId(), account.getItems().get(i).getName());					
+				}				
 				accountInfo = jsonMap.writeValueAsString(accountNameInfo);
-				System.out.println(accountInfo);
-			//	System.out.println(accountNameInfo.toString());
-				//System.out.println(list);
-				
+				System.out.println(accountInfo);	
 				
 		
 		}
